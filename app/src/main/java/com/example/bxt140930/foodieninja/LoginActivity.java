441fdf755413b374281a2b1d41bd7435c6410423
janju04 <jungwoo.jang@ca.com>
2016-10-29 10:47:06 -0500

    Adding Behnam's codes for UI

---
+++ app/src/main/java/com/example/bxt140930/foodieninja/LoginActivity.java
@@ -0,0 +1,28 @@
+package com.example.bxt140930.foodieninja;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button Login = (Button)findViewById(R.id.BTNLogin);
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
